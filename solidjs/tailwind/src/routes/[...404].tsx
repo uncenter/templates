284cd3c639b170d3b687f7e259b7eb337ac944a9
uncenter <me@uncenter.org>
2023-05-14 00:44:38 -0400

    feat: match solidjs/bare and solidjs/tailwind styling

--- solidjs/tailwind/src/routes/[...404].tsx
+++ solidjs/tailwind/src/routes/[...404].tsx
@@ -2,7 +2,7 @@ import { A } from "solid-start";
 
 export default function NotFound() {
     return (
-        <main class="flex flex-col justify-center text-center py-12">
+        <main class="text-center flex flex-col justify-center py-12">
             <p class="text-center text-gray-500 dark:text-gray-400">
                 The page you are looking for does not exist.
             </p>
--- solidjs/tailwind/src/routes/index.tsx
+++ solidjs/tailwind/src/routes/index.tsx
@@ -2,9 +2,9 @@ import Counter from "~/components/Counter";
 
 export default function App() {
     return (
-        <main class="lex flex-col justify-center text-center py-12">
+        <main class="text-center flex flex-col justify-center py-12">
             <h1 class="text-4xl font-bold">SolidStart + Tailwind CSS</h1>
-            <p class="text-gray-500 dark:text-gray-400">
+            <p class="text-gray-500 dark:text-gray-400 mt-2">
                 A SolidStart template with Tailwind CSS.
             </p>
             <Counter />
